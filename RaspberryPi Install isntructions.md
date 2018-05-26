1d6fe987f9c285c375f9c70734491c1ae534cf6d
jens <jens@lea.io>
2018-05-26 15:27:42 +0000

    Interval based listening

---
+++ RaspberryPi Install isntructions.md
@@ -0,0 +1,75 @@
+### install pocketsphinx via apt
+
+sudo apt install 
+
+
+
+## Install latest nodejs
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+
+## Node pocketshpinx module
+
+sudo apt install pocketsphinx libpocketsphinx-dev libsphinxbase-dev cmake pocketsphinx-en-us
+sudo npm install -g cmake-js
+sudo npm install pocketsphinx
+
+
+
+## runnign the command
+
+pocketsphinx_continuous -adcdev hw:1,0  -nfft 2048 -samprate 48000 -inmic yes
+pocketsphinx_continuous -adcdev hw:1,0 -inmic yes -samprate 44100 -nfft 2048
+pocketsphinx_continuous -inmic yes -samprate 44100 -nfft 2048
+
+pocketsphinx_continuous -inmic yes -samprate 44100 -nfft 2048 -allphone_ci 1 -lm language_model.lm -dict dictionary.dic
+
+pocketsphinx_continuous -adcdev plughw:1,0 -inmic yes -lm 6914.lm -dict 6914.dic -kws_delay 1 2>/dev/null
+
+pocketsphinx_continuous -adcdev plughw:1,0 -inmic yes -lm 0916.lm -dict 0916.dic -kws_delay 1 2>/dev/null
+
+
+## socat to unbuffer
+
+sudi apt install socat
+
+make a socket to pipe to
+$ mkfifo messages
+
+pocketsphinx_continuous -inmic yes -lm 0916.lm -dict 0916.dic -kws_delay 1 2>/dev/null
+
+socat EXEC:"listento",pty GOPEN:mylog.log
+
+
+
+## from recorded file
+
+
+
+pocketsphinx_continuous -infile test16k.wav -lm 6914.lm -dict 6914.dic 2>/dev/null
+
+
+## node library 
+
+https://github.com/eiriksm/pocketsphinx-continuous-node
+
+## generate the library files
+
+http://www.speech.cs.cmu.edu/tools/lmtool-new.html
+
+
+## setup alsa
+
+https://stackoverflow.com/questions/47188528/how-to-set-default-microphone-on-raspberry-pi-3b-with-raspbian-stretch
+
+
+
+## found code
+
+https://github.com/slowrunner/Pi3RoadTest
+
+
+---
+
+https://tlk.io/iotdundee
--- index.js
+++ index.js
@@ -1,61 +1,36 @@
-// var Psc = require('pocketsphinx-continuous');
-// var ps = new Psc({
-//   setId: '6914',  // A "set id". See explanation below.
-//   verbose: false // Setting this to true will give you a whole lot of debug output in your console.
-// });
-
-// ps.on("error", function(err) {
-// 	console.error("ERROR", err);
-// });
-
-// ps.on('data', function(data) {
-// 	console.log("DATA", data);
-// });
-
-// //ps.on('debug', function(data) {
-// //	console.error("Debug:", data.toString());
-// //});
-
 /// Spawning
-
-const { spawn } = require('child_process');
-
+// const { spawn } = require('child_process');
 // pocketsphinx_continuous -inmic yes -samprate 44100 -nfft 2048 -lm 6914.lm -dict 6914.dic
 // const ps = spawn('pocketsphinx_continuous', ['-inmic', 'yes', '-samprate', 44100, '-nfft', 2048, '-lm', '6914.lm', '-dict', '6914.dic']);
 
-// ps.stdout.on('data', (data) => {
-//   console.log(`stdout: ${data}`);
+// var readline = require('readline');
+// var rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+//   terminal: false
 // });
 
-// ps.stderr.on('data', (data) => {
-//   console.error(`stderr: ${data}`);
-// });
-
-// ps.on('close', (code) => {
-//   console.log(`child process exited with code ${code}`);
-// });
-
-/// Stdin Pipe
-
-// process.stdin.resume();
-// process.stdin.on('readable', () => {
-//   console.log("readable!")
-//   const chunk = process.stdin.read();
-//   if (chunk !== null) {
-//     process.stdout.write(`data: ${chunk}`);
+// rl.on('line', function(line){
+//   console.error("RAW Line", line);
+//   if (line !== "Listening..." && line !== "READY....") {
+//     console.log("Got", line);
 //   }
 // });
 
-// process.stdin.on('end', () => {
-//   process.stdout.write('end');
-// });
-var readline = require('readline');
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: true
-});
+/// Record, parse, readin, repeat
+function listen(loop = null) {
+  console.error("Listeing...")
+  const { execSync } = require('child_process');
+  const { readFileSync } = require('fs');
+  execSync('arecord --duration 3 -f s16_LE -r 16000 buffer.wav 2>/dev/null');
+  execSync('pocketsphinx_continuous -infile buffer.wav -lm 0916.lm -dict 0916.dic 2>/dev/null > buffer.txt');
+  const result = readFileSync('buffer.txt');
+  console.log("Understood:", result.toString());
+  if (loop) listen(loop);
+}
 
-rl.on('line', function(line){
-    console.log(line);
-})
+function run() {
+  listen(true); // start endless loop
+}
+
+run();
